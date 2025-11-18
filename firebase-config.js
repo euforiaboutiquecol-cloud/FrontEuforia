7ea8ffddb43952a7b537eba69d9d202682baffdc
euforiaboutiquecol-cloud <euforia.boutique.col@gmail.com>
2025-11-17 21:42:03 -0500

    Create firebase-config.js
    Prueba

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyD2nCryUttIjg3HaY9-r44rIZsDP1mLB5w",
+    authDomain: "euforia-inv.firebaseapp.com",
+    projectId: "euforia-inv",
+    storageBucket: "euforia-inv.firebasestorage.app",
+    messagingSenderId: "687723551803",
+    appId: "1:687723551803:web:a2a7b50fd78083883e501e",
+    measurementId: "G-CSMDCL57X6"
+};
+
+export const numPag = '+573219450153'
